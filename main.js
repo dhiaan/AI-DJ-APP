f107be1eb55c748bd73e66d11b1bf1cd09c526b7
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-11-09 17:45:33 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -54,6 +54,38 @@ function draw() {
 	fill("#FF0000");
 	stroke("#FF0000");
 
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY >0 && rightWristY <= 100)
+	{
+		document.getElementById("speed").innerHTML = "Speed = 0.5x";
+		song.rate(0.5); 
+	}
+
+    else if(rightWristY >100 && rightWristY <= 200)
+{
+	document.getElementById("speed").innerHTML = "Speed = 1x";
+	song.rate(1); 
+}
+
+else if(rightWristY >200 && rightWristY <= 300)
+{
+	document.getElementById("speed").innerHTML = "Speed = 1.5x";
+	song.rate(1.5); 
+}
+
+else if(rightWristY >300 && rightWristY <= 400)
+{
+	document.getElementById("speed").innerHTML = "Speed = 2x";
+	song.rate(2); 
+}
+
+else if(rightWristY >400 && rightWristY <= 500)
+{
+	document.getElementById("speed").innerHTML = "Speed = 2.5x";
+	song.rate(2.5); 
+}
+
 	if(scoreRightWrist > 0.2)
 	{ 
 		circle(rightWristX,rightWristY,20);
@@ -139,6 +171,7 @@ function gotPoses(results)
 {
   if(results.length > 0)
   {
+	  console.log(results);
 	scoreRightWrist =  results[0].pose.keypoints[10].score;
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
 	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
